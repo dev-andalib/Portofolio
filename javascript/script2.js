e46a83dc21849a90b93a23c5517e204555888a90
Andalib Iftakher <iftakherandalib@gmail.com>
2024-01-26 19:05:31 +0600

    Done with mobile view of project

--- javascript/script2.js
+++ javascript/script2.js
@@ -1,4 +1,13 @@
-$(function(){
-    var containerEl = document.querySelector(".mixit_main");
-    var mixer = mixitup(containerEl,{animation:{duration:1000}});
+$(function () {
+  var containerEl = document.querySelector(".mixit_main");
+  var mixer = mixitup(containerEl, { animation: { duration: 1000 } });
+
+  $(".stt").click(function () {
+    $("body, html").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
   });
+});
